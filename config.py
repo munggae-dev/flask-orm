6b5f04f3a6dc4adc433d8dbe2dbb898a7257821a
munggae-dev <nexaim95@gmail.com>
2023-06-09 14:50:59 +0900

    주석추가

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ BASE_DIR = os.path.dirname(__file__)
 
 config = dotenv_values(".env")
 
+# mysql
 SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{config["DB_USER"]}:{config["DB_PW"]}@{config["DB_HOST"]}:{config["DB_PORT"]}/{config["DB_NAME"]}?charset=utf8'
-# SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+
+# sqlite
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'test.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
